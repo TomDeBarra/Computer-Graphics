fe011ba92226b00ae0033d988c508b8c597337bf
TomDeBarra <Dylan.Barry@students.ittralee.ie>
2025-10-11 13:17:19 +0100

    Signed-off-by: TomDeBarra <Dylan.Barry@students.ittralee.ie>

--- GraphicsPipeline.cs
+++ GraphicsPipeline.cs
@@ -48,13 +48,6 @@ public class GraphicsPipeline : MonoBehaviour
             applyTransformation(imageAfterRotation, scaleMatrix);
 
         writeVectorsToFile(imageAfterScale, " After Scale (and Rotation)", " ---------------");
-        // old: Matrix4x4 viewingMatrix = Matrix4x4.LookAt(new Vector3(), new Vector3(), new Vector3());
-        
-        Matrix4x4 viewingMatrix = Matrix4x4.LookAt(
-            new Vector3(22, 5, 52),   // camera position
-            new Vector3(2, 2, 2),     // look at
-            new Vector3(3, 3, 20));   // up
-        Matrix4x4 projection = Matrix4x4.Perspective(90, 1, 1, 1000);
   
         // old: new Vector3()
         Matrix4x4 translationMatrix =
@@ -62,9 +55,81 @@ public class GraphicsPipeline : MonoBehaviour
                 Quaternion.identity,
                 Vector3.one);
         
-       // applyTransformation(verts, translationMatrix);
-       // Don't know if this is right
+        writeMatrixToFile(translationMatrix, " Translation Matrix ", " ---------------- ");
+
+        List<Vector4> imageAfterTranslation =
+            applyTransformation(imageAfterScale, translationMatrix);
+
+        writeVectorsToFile(imageAfterTranslation, " After Translation ", " ---------------");
+        
+        // old: Matrix4x4 viewingMatrix = Matrix4x4.LookAt(new Vector3(), new Vector3(), new Vector3());
+        Matrix4x4 viewingMatrix = Matrix4x4.LookAt(
+            new Vector3(22, 5, 52),   // camera position
+            new Vector3(2, 2, 2),     // look at
+            new Vector3(3, 3, 20));   // up
+
+        writeMatrixToFile(viewingMatrix, " Viewing Matrix ", " ---------------- ");
+        
+        List<Vector4> imageAfterViewing =
+            applyTransformation(imageAfterTranslation, viewingMatrix);
+
+        writeVectorsToFile(imageAfterViewing, " Image after Viewing Matrix ", " ---------------");
+        
+        // ----- Projection by Hand (Division) -----
+        List<Vector4> projectionByHand = new List<Vector4>();
+
+        foreach (Vector4 v in imageAfterViewing)
+        {
+            if (Mathf.Abs(v.z) > 1e-6f)
+            {
+                float xProj = v.x / v.z;
+                float yProj = v.y / v.z;
+                float zProj = v.z;
+                projectionByHand.Add(new Vector4(xProj, yProj, zProj, 1));
+            }
+            else
+            {
+                projectionByHand.Add(v);
+            }
+        }
+
+        // Write results to file
+        writeVectorsToFile(projectionByHand, " Projection by Hand (Division) ", " --------------- ");
+        
+        // ----- Final Image (After Manual Projection) -----
+        writeVectorsToFile(projectionByHand, " Final Image (Manual Projection) ", " --------------- ");
+
+        Matrix4x4 projection = Matrix4x4.Perspective(90, 1, 1, 1000);
+        writeMatrixToFile(projection, " Projection Matrix ", " ---------------- ");
+        
+        List<Vector4> finalImage =
+            applyTransformation(imageAfterViewing, projection);
+
+        writeVectorsToFile(finalImage, " Final Image (After Projection) ", " ---------------");
        
+        // Single Matrix of Transformations 
+        Matrix4x4 singleMatrix = translationMatrix * scaleMatrix * rotationMatrix;
+        
+        writeMatrixToFile(singleMatrix, " Single Matrix of Transformations ", " ---------------- ");
+        
+        List<Vector4> imageAfterSingleMatrix =
+            applyTransformation(verts, singleMatrix);
+        
+        writeVectorsToFile(imageAfterSingleMatrix, " Image after Single Matrix of Transformations ", " --------------- ");
+        
+        // Single Matrix for Everything 
+        Matrix4x4 singleMatrixEverything =
+            projection * viewingMatrix * translationMatrix * scaleMatrix * rotationMatrix;
+
+
+        writeMatrixToFile(singleMatrixEverything, " Single Matrix for Everything ", " ---------------- ");
+
+
+        List<Vector4> finalImageEverything =
+            applyTransformation(verts, singleMatrixEverything);
+        
+        writeVectorsToFile(finalImageEverything, " Final Image (Single Matrix for Everything) ", " --------------- ");
+
         writer.Close();
         
     }
@@ -112,7 +177,6 @@ public class GraphicsPipeline : MonoBehaviour
         { output.Add(tranformMatrix * v); }
 
         return output;
-
     }
 
     private void displayMatrix(Matrix4x4 rotationMatrix)
